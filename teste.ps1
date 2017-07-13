a40a7ee76362ea582d0ef16197928b9cfdb43493
Kenji Yasumitsu <fernando.kenji@cotriguacu.com.br>
2017-07-13 08:17:33 -0300

    Script para impressão automatizada
    Script para impressão automatizada

---
+++ teste.ps1
@@ -0,0 +1,12 @@
+﻿
+
+if ((Get-ChildItem -Force D:\teste).Count) {
+    #Start-Process -FilePath "C:\Users\fernando.kenji\Documents\testeteste.pdf" 
+    #–Verb Print -PassThru | %{sleep 10;$_} | kill;
+    Remove-Item D:\teste\*.* -Recurse
+    "removido com sucesso"
+}
+else
+{
+    "não existem arquivos"
+}
